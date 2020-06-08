2f746a66012989764117eef4fdd6055cf700ae2c
Nastya Kvitko <49814777+NastyaKvitko@users.noreply.github.com>
2020-06-08 15:58:39 +0300

    Update login.php

--- login.php
+++ login.php
@@ -8,38 +8,31 @@ $password = $_POST["password"];
  
 
 for ($i = 0; $i < count($users); $i++)
-{	
+{  
     if (($login == $users[$i]['login']) && ($password == $users[$i]['password']))
     {
-	        $_SESSION['login'] = $users[$i]['login'];
-	        $_SESSION['password'] = $users[$i]['password'];
+          $_SESSION['login'] = $users[$i]['login'];
+          $_SESSION['password'] = $users[$i]['password'];
+          $_SESSION['surname'] = $users[$i]['surname'];
+        $_SESSION['lang'] = $users[$i]['lang'];     
+        $_SESSION['role'] = $users[$i]['role'];
+        $_SESSION['name'] = $users[$i]['name'];     
 
- 			if ($users[$i]['role'] == 'client'){	            
-	            $_SESSION['surname'] = $users[$i]['surname'];
-	            $_SESSION['lang'] = $users[$i]['lang'];
-	            $_SESSION['role'] = $users[$i]['role'];
-	            $_SESSION['name'] = $users[$i]['name'];
-	            header('Location: users\client.php');
-	        }	        
 
-	        if ($users[$i]['role'] == 'manager')
-	        {
-		            $_SESSION['surname'] = $users[$i]['surname'];
-		            $_SESSION['lang'] = $users[$i]['lang'];	   
-		            $_SESSION['role'] = $users[$i]['role'];
-		            $_SESSION['name'] = $users[$i]['name'];
-		            header('Location: users\manager.php'); 
-	        }
+       if ($users[$i]['role'] == 'client')
+       {              
+              header('Location: users\client.php');
+          }          
 
-			if ($users[$i]['role'] == 'admin')
-	        {		            
-		            $_SESSION['surname'] = $users[$i]['surname'];
-		            $_SESSION['lang'] = $users[$i]['lang'];	   
-		            $_SESSION['role'] = $users[$i]['role'];
-		            $_SESSION['name'] = $users[$i]['name'];     
-		            header('Location: users\admin.php');
-	        }       
+          if ($users[$i]['role'] == 'manager')
+          {
+             header('Location: users\manager.php'); 
+          }
+
+      if ($users[$i]['role'] == 'admin')
+          {                
+              header('Location: users\admin.php');
+          }       
 
     }
 }
-
